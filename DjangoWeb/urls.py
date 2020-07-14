02b463548ea71461694ef31d18489c0dbbb52683
Luis Miguel Diaz <luis.miguel.diaz@outlook.com>
2020-07-14 18:14:51 -0400

    Update login/logout

--- DjangoWeb/urls.py
+++ DjangoWeb/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.conf.urls import include
 
 
 urlpatterns = [
@@ -25,6 +26,15 @@ urlpatterns = [
              }
          ),
          name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', 
+         LogoutView.as_view
+         (
+             template_name='app/loggedoff.html', 
+             extra_context=
+             {
+                 'year': datetime.now().year
+                 }
+             ), 
+             name='logout'),
     path('admin/', admin.site.urls),
 ]
